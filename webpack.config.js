4130fc49c8dbbc1341d361102efe0f91128b9e79
Aditya Chalikwar <adityachalikwar@gmail.com>
2020-05-21 17:34:16 +0530

    Setup dev-depensies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env) => {
     return ({
         entry : './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module : {
@@ -36,8 +36,9 @@ module.exports = (env) => {
         plugins: [ CSSExtract ],
         devtool: env === 'production' ? 'source-map' :'inline-source-map' ,
         devServer: {
-            contentBase: "./public",
-            historyApiFallback: true 
+            contentBase: "./public/",
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     })
 }
